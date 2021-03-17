4d9aacc7ef3e17e8ed82f391046a417edc2e795d
konh <konh@yandex.ru>
2021-03-17 07:32:33 +0700

    Use authorized access to test controller

--- InvestmentReporting.TestService/Controllers/HelloController.cs
+++ InvestmentReporting.TestService/Controllers/HelloController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace InvestmentReporting.TestService.Controllers {
+	[Authorize]
 	[ApiController]
 	[Route("api/test/v1/[controller]")]
 	public class HelloController : ControllerBase {
