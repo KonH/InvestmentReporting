a514433455e6cfb6adead85165bf025f07d76fb6
konh <konh@yandex.ru>
2021-03-18 19:04:11 +0700

    Proper way to handle development mode with API services from docker

--- Frontend/src/service/backend.ts
+++ Frontend/src/service/backend.ts
@@ -1,7 +1,4 @@
 export default class Backend {
-	// TODO: replace url in prod
-	private static url = 'http://localhost:8082/';
-
 	static async post(relativeUrl: string, init?: RequestInit) {
 		init = init ?? {};
 		init.method = 'POST';
@@ -9,11 +6,8 @@ export default class Backend {
 	}
 
 	static async fetch(relativeUrl: string, init?: RequestInit) {
-		init = init ?? {};
-		// TODO: do not use no-cors in prod
-		init.mode = 'no-cors';
-		const result = await fetch(this.url + relativeUrl, init);
-		if (!result.ok) {
+		const result = await fetch(relativeUrl, init);
+		if (process.env.NODE_ENV == 'development') {
 			console.log(result);
 		}
 		return result;
--- Frontend/vue.config.js
+++ Frontend/vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+	devServer: {
+		proxy: 'http://localhost:8082',
+	},
 	chainWebpack: (config) => {
 		config.plugin('html').tap((args) => {
 			args[0].title = 'Investment Reporting';
