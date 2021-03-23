5e58695eb97607de64b7704785152fbd9562f190
konh <konh@yandex.ru>
2021-03-23 20:32:57 +0700

    Refactor build process

--- BuildSystem/Build.cs
+++ BuildSystem/Build.cs
@@ -10,6 +10,11 @@ using static Nuke.Common.Tools.DotNet.DotNetTasks;
 namespace InvestmentReporting.BuildSystem {
 	[UnsetVisualStudioEnvironmentVariables]
 	class Build : NukeBuild {
+		static readonly (string, string)[] DotNetProjects = {
+			("InvestmentReporting.AuthService", "auth.ts"),
+			("InvestmentReporting.InviteService", "invite.ts")
+		};
+
 		[Parameter]
 		string Configuration = "Debug";
 
@@ -22,8 +27,9 @@ namespace InvestmentReporting.BuildSystem {
 					RootDirectory,
 					"dotnet", "tool install --global Swashbuckle.AspNetCore.Cli --version 5.4.1",
 					ignoreExitCode: true);
-				DotNetRestore(s => s.SetProjectFile(RootDirectory / "InvestmentReporting.AuthService"));
-				DotNetRestore(s => s.SetProjectFile(RootDirectory / "InvestmentReporting.InviteService"));
+				foreach ( var (project, _) in DotNetProjects ) {
+					DotNetRestore(s => s.SetProjectFile(RootDirectory / project));
+				}
 				Run("Restoring frontend packages",
 					RootDirectory / "Frontend",
 					"npm", "install");
@@ -37,58 +43,41 @@ namespace InvestmentReporting.BuildSystem {
 				var architecture = RuntimeInformation.ProcessArchitecture;
 
 				var dotNetPlatform = GetDotNetBuildArchitecture(architecture);
-				DotNetBuild(s => s
-					.SetProjectFile(RootDirectory / "InvestmentReporting.AuthService")
-					.SetConfiguration(Configuration));
-				DotNetBuild(s => s
-					.SetProjectFile(RootDirectory / "InvestmentReporting.InviteService")
-					.SetConfiguration(Configuration));
+				foreach ( var (project, _) in DotNetProjects ) {
+					DotNetBuild(s => s
+						.SetProjectFile(RootDirectory / project)
+						.SetConfiguration(Configuration));
+				}
 
 				if ( Configuration == "Debug" ) {
 					Environment.SetEnvironmentVariable("SWAGGER_RUN", true.ToString());
 					try {
 						var apiDir = RootDirectory / "api";
 						EnsureExistingDirectory(apiDir);
-						{
-							var swaggerPath = apiDir / "InvestmentReporting.AuthService.swagger.json";
-							var dllPath     = RootDirectory / "InvestmentReporting.AuthService" / "bin" / Configuration / "net5.0" / "InvestmentReporting.AuthService.dll";
+						foreach ( var (project, api) in DotNetProjects ) {
+							var swaggerPath = apiDir / $"{project}.swagger.json";
+							var dllPath     = RootDirectory / project / "bin" / Configuration / "net5.0" / $"{project}.dll";
 							Run("Generate swagger api file",
-								RootDirectory / "InvestmentReporting.AuthService",
+								RootDirectory / project,
 								"swagger", $"tofile --output {swaggerPath} {dllPath} v1");
 							Run("Generate api client from swagger file",
 								RootDirectory / "Frontend",
-								"npm", "run generate-api -- --path ../api/InvestmentReporting.AuthService.swagger.json --output src/api --name \"auth.ts\"");
-						}
-						{
-							var swaggerPath = apiDir / "InvestmentReporting.InviteService.swagger.json";
-							var dllPath     = RootDirectory / "InvestmentReporting.InviteService" / "bin" / Configuration / "net5.0" / "InvestmentReporting.InviteService.dll";
-							Run("Generate swagger api file",
-								RootDirectory / "InvestmentReporting.InviteService",
-								"swagger", $"tofile --output {swaggerPath} {dllPath} v1");
-							Run("Generate api client from swagger file",
-								RootDirectory / "Frontend",
-								"npm", "run generate-api -- --path ../api/InvestmentReporting.InviteService.swagger.json --output src/api --name \"invite.ts\"");
+								"npm", $"run generate-api -- --path ../api/{project}.swagger.json --output src/api --name \"{api}\"");
 						}
 					} finally {
 						Environment.SetEnvironmentVariable("SWAGGER_RUN", false.ToString());
 					}
 				}
 
-				DotNetPublish(s => s
-					.SetProject(RootDirectory / "InvestmentReporting.AuthService")
-					.SetConfiguration(Configuration)
-					.SetRuntime($"linux-musl-{dotNetPlatform}")
-					.EnablePublishSingleFile()
-					.EnableSelfContained()
-					.SetOutput(RootDirectory / "InvestmentReporting.AuthService" / "publish"));
-
-				DotNetPublish(s => s
-					.SetProject(RootDirectory / "InvestmentReporting.InviteService")
-					.SetConfiguration(Configuration)
-					.SetRuntime($"linux-musl-{dotNetPlatform}")
-					.EnablePublishSingleFile()
-					.EnableSelfContained()
-					.SetOutput(RootDirectory / "InvestmentReporting.InviteService" / "publish"));
+				foreach ( var (project, _) in DotNetProjects ) {
+					DotNetPublish(s => s
+						.SetProject(RootDirectory / project)
+						.SetConfiguration(Configuration)
+						.SetRuntime($"linux-musl-{dotNetPlatform}")
+						.EnablePublishSingleFile()
+						.EnableSelfContained()
+						.SetOutput(RootDirectory / project / "publish"));
+				}
 
 				Run("Building frontend",
 					RootDirectory / "Frontend",
