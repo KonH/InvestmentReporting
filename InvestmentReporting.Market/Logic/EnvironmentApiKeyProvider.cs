5e4ba786b8454e4b23b4b74289d426ff202b59b6
konh <konh@yandex.ru>
2021-04-27 07:12:24 +0700

    Add verbose error about missing API key

--- InvestmentReporting.Market/Logic/EnvironmentApiKeyProvider.cs
+++ InvestmentReporting.Market/Logic/EnvironmentApiKeyProvider.cs
@@ -1,11 +1,17 @@
 using System;
+using Microsoft.Extensions.Logging;
 
 namespace InvestmentReporting.Market.Logic {
 	public sealed class EnvironmentApiKeyProvider : IApiKeyProvider {
 		public string ApiKey { get; }
 
-		public EnvironmentApiKeyProvider() {
+		public EnvironmentApiKeyProvider(ILogger<EnvironmentApiKeyProvider> logger) {
 			ApiKey = Environment.GetEnvironmentVariable("TINKOFF_API_SANDBOX_KEY") ?? string.Empty;
+			if ( string.IsNullOrWhiteSpace(ApiKey) ) {
+				logger.LogError(
+					"Please provide valid TINKOFF_API_SANDBOX_KEY environment variable " +
+					"(see https://tinkoffcreditsystems.github.io/invest-openapi/auth/)");
+			}
 		}
 	}
 }
