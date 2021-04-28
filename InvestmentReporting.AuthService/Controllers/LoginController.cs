00469b31326defe07ee3838c7712daa670547d90
konh <konh@yandex.ru>
2021-04-28 18:58:59 +0700

    Add random delay to decrease chance of time-based attacks

--- InvestmentReporting.AuthService/Controllers/LoginController.cs
+++ InvestmentReporting.AuthService/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using InvestmentReporting.Shared.Models;
 using Microsoft.AspNetCore.Http;
@@ -10,14 +11,18 @@ namespace InvestmentReporting.AuthService.Controllers {
 	[ApiController]
 	[Route("[controller]")]
 	public class LoginController : ControllerBase {
+		static readonly TimeSpan _maxRandomDelay = TimeSpan.FromSeconds(0.25);
+
 		readonly ILogger             _logger;
 		readonly UserManager<User>   _userManager;
 		readonly SignInManager<User> _signInManager;
+		readonly Random              _random;
 
 		public LoginController(ILogger<LoginController> logger, UserManager<User> userManager, SignInManager<User> signInManager) {
 			_logger        = logger;
 			_userManager   = userManager;
 			_signInManager = signInManager;
+			_random        = new Random();
 		}
 
 		[HttpPost]
@@ -28,15 +33,20 @@ namespace InvestmentReporting.AuthService.Controllers {
 			var user = await _userManager.FindByNameAsync(userName);
 			_logger.LogInformation($"{nameof(Login)}: found user '{user?.Id}'");
 			if ( user == null ) {
+				await SafetyDelay();
 				return BadRequest();
 			}
 			var result = await _signInManager.PasswordSignInAsync(user, password, true, false);
 			if ( !result.Succeeded ) {
 				_logger.LogWarning($"{nameof(Login)}: failed login: {result}");
+				await SafetyDelay();
 				return BadRequest();
 			}
 			_logger.LogInformation($"{nameof(Login)}: login success for '{user.Id}'");
 			return Ok();
 		}
+
+		async Task SafetyDelay() =>
+			await Task.Delay(_random.Next(_maxRandomDelay.Milliseconds));
 	}
 }
